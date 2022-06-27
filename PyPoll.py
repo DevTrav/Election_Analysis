def21bd265d3fdd61236ea1b2a288ae7ee7efde1
Travers La Ville <traverslaville@traverss-mbp.lan>
2022-06-26 21:08:16 -0500

    Added conditional to add candidates to candidate.options

--- PyPoll.py
+++ PyPoll.py
@@ -6,12 +6,31 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total votes counter
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-    # Read the file object with reader function from csv module 
     file_reader = csv.reader(election_data)
 
 # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+
+#Print ech row in the CSV file
+    for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+
+        # Print the tcandidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match existing candidate
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+# Print Candidate Options list
+print(candidate_options)
